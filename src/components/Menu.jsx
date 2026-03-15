523d58dbca04daf4e8c13e150403ff2679281476
Aashish Chalise <aashishchalise01@gmail.com>
2026-03-15 14:13:00 +0000

    added dialog

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { useState } from "react";
 import { foodMenu } from "../assets/assets";
 import item1Img from "../assets/item1.avif";
+import { ImCross } from "react-icons/im";
 
 const Menu = () => {
+  const [dialog, setDialog] = useState([false, {}]);
+
+  const activateDialog = (item) => {
+    console.log("triggeredd");
+    setDialog([true, item]);
+  };
+  const disableDialog = () => {
+    setDialog([false, {}]);
+  };
   return (
     <div className="mt-10  bg-[#F8F0E3] pb-10">
       <div className="container mx-auto max-w-[1030px] ">
@@ -20,10 +31,14 @@ const Menu = () => {
             return (
               <div
                 key={index}
-                className="flex group relative flex-col  shadow-xl overflow-hidden  bg-[#F8F0E3] rounded-2xl  hover:scale-103 transition mb-5 "
+                className="flex group relative flex-col  shadow-xl overflow-hidden  bg-[#F8F0E3] rounded-2xl
+                  hover:scale-103 transition mb-5 "
+                onClick={() => {
+                  activateDialog(item);
+                }}
               >
                 {/* The Overlay */}
-                <div className="inset-0  absolute opacity-0 z-[40]  bg-black/0 group-hover:bg-black/15  group-hover:opacity-100   transition-all duration-300 ease">
+                <div className="inset-0  absolute opacity-0 z-[20]  bg-black/0 group-hover:bg-black/15  group-hover:opacity-100   transition-all duration-300 ease">
                   {/* The mouse will ignore this layer entirely! */}
                 </div>
                 <img
@@ -48,6 +63,29 @@ const Menu = () => {
       <button className="px-3 py-3 text-white bg-primary rounded-full hover:bg-red-800 w-40 mt-10 mx-auto block">
         View Full Menu
       </button>
+      {dialog[0] === true ? (
+        <div className="fixed top-0 w-full h-screen bg-black/50 flex justify-center items-center px-5 z-30">
+          <div className="container mx-auto flex flex-col max-w-xl px-5  gap-3 py-7 shadow-xl bg-white rounded-xl relative">
+            <div className="wrapper relative">
+              <div
+                className="flex justify-center items-center w-8 h-8  
+              absolute top-[-12px] right-[-11px] border border-white bg-primary rounded-full text-md text-white"
+                onClick={disableDialog}
+              >
+                <ImCross />
+              </div>
+              <img
+                src={dialog[1].image}
+                alt=""
+                className="max-h-50 rounded-xl object-cover w-full"
+              />
+            </div>
+            <h3 className="text-2xl">{dialog[1].title}</h3>
+            <p>{dialog[1].description}</p>
+            <span className="text-2xl ">{dialog[1].price}</span>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
